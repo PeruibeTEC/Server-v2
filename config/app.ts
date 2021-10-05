0f3dc1698e44a9561245d02d0f4c7acad4f1ae21
hmartiins <hmartins224@gmail.com>
2021-10-05 11:10:36 -0300

    feat: create Adonis project

---
+++ config/app.ts
@@ -0,0 +1,59 @@
+import proxyAddr from 'proxy-addr'
+import Env from '@ioc:Adonis/Core/Env'
+
+import { ServerConfig } from '@ioc:Adonis/Core/Server'
+import { LoggerConfig } from '@ioc:Adonis/Core/Logger'
+import { ProfilerConfig } from '@ioc:Adonis/Core/Profiler'
+import { ValidatorConfig } from '@ioc:Adonis/Core/Validator'
+
+export const appKey: string = Env.get('APP_KEY')
+
+/*
+|--------------------------------------------------------------------------
+| Http server configuration
+|--------------------------------------------------------------------------
+|
+| The configuration for the HTTP(s) server. Make sure to go through all
+| the config properties to make keep server secure.
+|
+*/
+export const http: ServerConfig = {
+  allowMethodSpoofing: false,
+  subdomainOffset: 2,
+
+  generateRequestId: false,
+
+  trustProxy: proxyAddr.compile('loopback'),
+
+  etag: false,
+
+  jsonpCallbackName: 'callback',
+
+  cookie: {
+    domain: '',
+    path: '/',
+    maxAge: '2h',
+    httpOnly: true,
+    secure: false,
+    sameSite: false,
+  },
+
+  forceContentNegotiationTo: 'application/json',
+}
+
+export const logger: LoggerConfig = {
+  name: Env.get('APP_NAME'),
+  enabled: true,
+
+  level: Env.get('LOG_LEVEL', 'info'),
+  prettyPrint: Env.get('NODE_ENV') === 'development',
+}
+
+export const profiler: ProfilerConfig = {
+  enabled: true,
+
+  blacklist: [],
+  whitelist: [],
+}
+
+export const validator: ValidatorConfig = {}
---
+++ start/kernel.ts
@@ -0,0 +1,4 @@
+import Server from '@ioc:Adonis/Core/Server'
+
+Server.middleware.register([() => import('@ioc:Adonis/Core/BodyParser')])
+Server.middleware.registerNamed({})
